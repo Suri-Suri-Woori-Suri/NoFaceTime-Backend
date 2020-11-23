25a7aba58d68437ec9a7d290222a25dd9b0136c5
woori9 <woori9@users.noreply.github.com>
2020-11-23 11:45:05 +0900

    [Feat] : added socket.io event for Chat
    I added socket.io logic to enable chatting between users in a room

--- bin/www.js
+++ bin/www.js
@@ -27,35 +27,31 @@ const io = socketio(server, {
   }
 });
 
-// const socketFunc = require('../utils/socket');
-// const { disconnect } = require('process');
-// socketFunc.startSocket(io);
 const members = {};  // socketId : { userId, roomId, nickname, socketId }
 const rooms = {}; //id : { host, memberList }
 
 io.on('connection', socket => {
   console.log('we have a new connection!!');
   //나중에 host 정보도 가져와야한다.- > secret chat 구현위해서
-  socket.on('join-room', ({ roomId, userId, nickname }) => {//userId === socket.id
+  socket.on('join-room', ({ roomId, userId, nickname, host }) => {//userId === socket.id
     socket.join(roomId);
+    console.log('YOU JOINED A ROOM!')
 
     if (!rooms.hasOwnProperty(roomId)) rooms[roomId] = { memberList: [] };//host: socket.id
+    if (host) rooms[roomId].host = socket.id;
+    console.log(rooms);
     //if (host === true) //예시
     //rooms[roomId] = socket.id//(host's socket Id)
     //socket.to(roomId).emit('joined', { members });
-    const newobj= {};
+    const newobj = {};
     const newMember = { roomId, userId, nickname, socketId: socket.id };
     newobj[socket.id] = newMember;
-    console.log("$$$", newobj);
 
     io.to(socket.id).emit('joined', { members });//기존의 맴버, 방금 join 한 사람한테만간다
     socket.to(roomId).emit('joined', { newMember: newobj });//새 맴버, 방금 join 한 사람 외의 모두에게 간다.
 
     members[socket.id] = newMember;
     rooms[roomId].memberList.push(newMember);
-    console.log(rooms);
-    console.log(members)
-
     //io.in(roomId).emit('joined', { newMember, members }) //sender 포함
     //socket.broadcast.to(roomId).emit('joined', { newMember, members });
   });
@@ -75,20 +71,58 @@ io.on('connection', socket => {
   socket.on('respond signal', ({ signal, to }) => {
     console.log('respond signal(SERVER)', to);
     const from = members[socket.id];
-    console.log("RECIVER", from);//'xfMyb6PCqSQ8PKN8AAAD', 08xBsNi1UOtMl26hAAAF'
-    console.log("INITIATOR", to);//'08xBsNi1UOtMl26hAAAF'
+    console.log("RECIVER", from);
+    console.log("INITIATOR", to);
     const { socketId } = to;
 
     io.to(socketId).emit('respond signal', { signal, from });
   });
+
+  /////////////// Chat
+  socket.on('message-public', message => {
+    socket.join('test');//나중에 룸 아이디로 변경. 테스트용
+    console.log(user)
+    const user = members[socket.id];// 나중에 주석풀기
+    //const targetRoom = user.roomId; 나중에 주석 풀기
+    //io.to(targetRoom)
+    const targetRoom = 'test';
+    console.log(message);
+    console.log(socket.id)
+
+    io.in(targetRoom).emit('message-public', { text: message, from: user.nickname });
+  });
+
+  socket.on('message-secret', message => {
+    const user = members[socket.id];//
+    //const targetRoom = user.roomId; 나중에 주석 풀기
+    const targetRoom = 'test';
+    io.to(socketId).emit('message-secret', { text: message, from: user.nickname });
+  });
+
+
+  socket.on('outof-chat', () => {
+
+  });
+
+  ///////////////  Leave
   socket.on('leave', (data) => {
-    console.log('someone leave..');//버튼 눌러서
+    console.log('someone leave..  Button');//버튼 눌러서
     //socket.to(socket.id).emit('user left', socket.id);
   });
 
   socket.on('disconnect', () => {
-    console.log('someone leave..');//창 닫아버림
-    socket.to(socket.id).emit('user left', { sockeId: socket.id });
+    console.log('someone leave..  X');//창 닫아버림
+    // const members = {}; socketId : { userId, roomId, nickname, socketId }
+    // const rooms = {}; id : { host, memberList }
+    const roomId = members[socket.id].roomId;
+
+    delete members[socket.id];
+    const updatedMember = rooms[roomId].memberList.filter(member => member.socketId !== socket.id);
+    rooms[roomId].memberList = updatedMember;
+
+    if (members[socketId]) {
+      socket.to(roomId).emit('user left', { socketId: socket.id });// to room except sender
+    }
   });
 });
 
